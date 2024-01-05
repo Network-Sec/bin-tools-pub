d94db2766b8c3662e27e87cfac70ba47c60c1401
Network Sec <85315993+Network-Sec@users.noreply.github.com>
2024-01-05 15:05:16 +0100

    Update gen_letter_digit_special_chars.py

--- gen_letter_digit_special_chars.py
+++ gen_letter_digit_special_chars.py
@@ -3,10 +3,13 @@ import string
 
 # Create a list of uppercase & lowercase chars and digits
 # a...a!...a!!...z...z!...z!!...aa...aa!...aa!!...ab...ab!...ab!!
-# In the example is only one special char (exclamation) but below we use all common 
-# special chars used in passwords. hashcat doesn't provide this unique combination
-# and erroers when we try to generate such a list with --stdout. There's custom
-# combos like -1 ?l?u?d but the custom charset isn't supported in all modes.
+# In the example is only one special char (exclamation mark) but the script uses all common 
+# special chars used in passwords. 
+
+# hashcat doesn't provide this unique combination # and erroers when we try to generate such a list with --stdout. 
+# .\hashcat.exe -d 2 -i -a 3 -1 ?l?u?d ?1?1?1?1?1 --stdout > UpLowDig5.txt -w 3
+# OperationStopped: (:) [], OutOfMemoryException
+# There's custom combos like -1 ?l?u?d but the custom charset isn't supported in all modes.
 # That's why I created this little tool. Have some disk space and time ready. 
 
 chars_digits = list(string.ascii_letters + string.digits)
