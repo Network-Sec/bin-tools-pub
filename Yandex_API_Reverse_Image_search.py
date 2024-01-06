b33a94a8055f7308236f3cdd9a0b600e555ad9b2
Network Sec <85315993+Network-Sec@users.noreply.github.com>
2024-01-06 22:56:29 +0100

    Update Yandex_API_Reverse_Image_search.py
    Corrected errors

--- Yandex_API_Reverse_Image_search.py
+++ Yandex_API_Reverse_Image_search.py
@@ -6,17 +6,30 @@ import json
 
 # Query should be a public url with the image to reverse search
 query = ""
+maxResultPages = 50
+folder = "/tmp/img/"
 
 # SerpAPI key
 serpAPIkey = ""
 
-maxResultPages = 50
+def download_image(image_url, folder_path):
+    try:
+        response = requests.get(image_url, stream=True)
+        if response.status_code == 200:
+            file_path = os.path.join(folder_path, image_url.split("/")[-1])
+            with open(file_path, 'wb') as f:
+                for chunk in response.iter_content(1024):
+                    f.write(chunk)
+            return True
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return False
 
 def yandex_image_search(api_key, query, max_pages):
     base_url = "https://serpapi.com/search.json"
     params = {
-        "engine": "yandex",
-        "text": query,
+        "engine": "yandex_images",
+        "url": query,
         "api_key": api_key,
         "output": "json"
     }
@@ -34,9 +47,11 @@ def yandex_image_search(api_key, query, max_pages):
         with open("yand.json" + now.strftime("%Y-%m-%d_%H-%M-%S"), "w") as f:
             f.write(json.dumps(data, indent=4))
 
-        if "organic_results" in data:
-            for result in data["organic_results"]:
-                if "link" in result:
-                    print(result["link"])
+        for image_result in data["image_results"]:
+            print(image_result["title"])
+            print("Width", image_result["original_image"]["width"], " Height", image_result["original_image"]["height"])
+            imgurl = image_result["original_image"]["link"]
+            print(imgurl)
+            download_image(imgurl, folder)
                   
 yandex_image_search(serpAPIkey, query, maxResultPages)
