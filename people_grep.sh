1e6a26219993f963352bb891e501bc8a3f89d026
Network Sec <85315993+Network-Sec@users.noreply.github.com>
2024-02-19 20:37:39 +0100

    Update people_grep.sh
    Added -s for very small search range
    Added optional folder argument (last arg)

--- people_grep.sh
+++ people_grep.sh
@@ -4,12 +4,16 @@
 # for subfolders, looking for all .txt files
 datafolder="/mnt/d/datafolder/"
 narrowSearch=false
+smallSearch=false
 
-# Check for optional '-n' parameter
-while getopts ":n" opt; do
+# Check for '-n' or '-s' parameters
+while getopts ":ns" opt; do
   case $opt in
     n)
       narrowSearch=true
+      ;;
+    s)
+      smallSearch=true
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -21,8 +25,8 @@ done
 # Remove the options from the positional parameters
 shift $((OPTIND-1))
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 [-n] firstName lastName"
+if [ "$#" -le 1 ]; then
+    echo "Usage: $0 [-n|-s] firstName lastName <optional folder>"
     exit 1
 fi
 
@@ -35,24 +39,31 @@ noSeparator=""
 
 patterns=()
 
-# Function to add patterns
+# Function to add patterns including both name directions
 add_pattern() {
     local fName=$1
     local lName=$2
+    # First name followed by last name
     patterns+=("${fName}${separators}${lName}")
-    patterns+=("${fName}${noSeparator}${lName}") # Case with no special char
+    patterns+=("${fName}${noSeparator}${lName}")
+    # Last name followed by first name
+    patterns+=("${lName}${separators}${fName}")
+    patterns+=("${lName}${noSeparator}${fName}")
 }
 
 # Narrow search patterns
 if [ "$narrowSearch" = true ]; then
-    # Edge case with optional special char and no special char
     add_pattern "${firstName:0:1}" "$lastName"
-    # Full name with optional special char and no special char
+    add_pattern "$firstName" "$lastName"
+elif [ "$smallSearch" = true ]; then
+    # For small search, only use complete first and last name with and without separators
     add_pattern "$firstName" "$lastName"
 else
     # General search logic
-    patterns+=("${firstName:0:1}${separators}${lastName}") # Special edge case
-    patterns+=("${firstName:0:1}${noSeparator}${lastName}") # Without special char
+    patterns+=("${firstName:0:1}${separators}${lastName}")
+    patterns+=("${firstName:0:1}${noSeparator}${lastName}")
+    patterns+=("${lastName}${separators}${firstName:0:1}")
+    patterns+=("${lastName}${noSeparator}${firstName:0:1}")
     for i in {1..9}; do
         fNameLen=$(( (${#firstName} * i + 9) / 10 ))
         lNameLen=$(( (${#lastName} * (10 - i) + 9) / 10 ))
@@ -67,7 +78,11 @@ else
         lNameSlice="${lastName:0:$lNameLen}"
         add_pattern "$fNameSlice" "$lNameSlice"
     done
-    add_pattern "$firstName" "$lastName" # Full name case
+    add_pattern "$firstName" "$lastName"
+fi
+
+if [ -n "$3" ]; then
+    datafolder="$3"
 fi
 
 # Process patterns and enable colored output
