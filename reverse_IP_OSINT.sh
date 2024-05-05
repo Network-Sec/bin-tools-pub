d4731df2cfc8aeaa5e4589cb0f14fcf0bdc2b723
Network Sec <85315993+Network-Sec@users.noreply.github.com>
2024-05-05 13:09:16 +0200

    Update reverse_IP_OSINT.sh

--- reverse_IP_OSINT.sh
+++ reverse_IP_OSINT.sh
@@ -4,7 +4,6 @@
 # provide practical Threat Intelligence Output
 # Add more services as needed
 
-# Usage check
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <IP Address or CIDR>"
     exit 1
@@ -13,11 +12,10 @@ fi
 INPUT="$1"
 IS_CIDR=false
 
-# Function to process a single IP
 process_ip() {
     local IP=$1
 
-    # Skip network and broadcast addresses typically ending in .0 or .255
+    
     if [[ "$IP" =~ \.0$ || "$IP" =~ \.255$ ]]; then
         echo "Skipping network/broadcast address: $IP"
         return
@@ -25,56 +23,56 @@ process_ip() {
 
     echo "----------------- Processing IP: $IP -----------------"
 
-    # Reverse DNS lookup
+    
     echo "[+] dig result for $IP"
     dig -x $IP +short @8.8.8.8
     echo ""
 
-    # Domain information from site.ip138.com
+    
     echo "[+] Domain information from site.ip138.com for $IP"
     timeout 10 curl -s "https://site.ip138.com/$IP" -H "User-Agent: Mozilla/5.0" | pup 'div.result.result2 ul#list li a[href] text{}'
     echo ""
 
-    # Domain information from ipchaxun.com
+    
     echo "[+] Domain information from ipchaxun.com for $IP"
     timeout 10 curl -s "https://ipchaxun.com/$IP/" -H "User-Agent: Mozilla/5.0" | pup 'div#J_domain p a text{}'
     echo ""
 
     if [[ $IS_CIDR=false ]]; 
     then 
-        # tlsx for SAN and CN from the certificate
+        
         echo "[+] SAN and CN from the certificate for $IP"
         echo $IP | tlsx -san -cn -silent
         echo ""
 
-        # Domain information from rapiddns.io
+        
         echo "[+] Domain information from rapiddns.io for $IP"
         timeout 10 curl -s "https://rapiddns.io/s/$IP" -H "User-Agent: Mozilla/5.0" | pup 'div#result div.row div.col-lg-12 table#table tbody tr td:nth-of-type(1) text{}'
         echo ""
     fi
 }
 
-# Function to handle CIDR for services supporting it directly
+
 process_cidr() {
     local CIDR=$1
-    echo "Using direct CIDR support for services."
+    # echo "Using direct CIDR support for services."
 
-    # tlsx for SAN and CN from the certificate
+    
     echo "[+] SAN and CN from the certificate for $CIDR"
     echo $CIDR | tlsx -san -cn -silent
     echo ""
 
-    # Domain information from rapiddns.io
+    
     echo "[+] Domain information from rapiddns.io for $CIDR"
     timeout 10 curl -s "https://rapiddns.io/s/$CIDR" -H "User-Agent: Mozilla/5.0" | pup 'div#result div.row div.col-lg-12 table#table tbody tr td:nth-of-type(1) text{}'
     echo ""
 }
 
-# Function to generate IP addresses from CIDR
+
 expand_cidr() {
     local cidr=$1
 
-    # Convert IP to integer
+    
     local baseip=$(echo $cidr | cut -d/ -f1)
     local mask=$(echo $cidr | cut -d/ -f2)
     local ip_int=0
@@ -85,7 +83,7 @@ expand_cidr() {
     done
 
     local num_ips=$((1 << (32 - mask)))
-    for i in $(seq 1 $((num_ips - 2))); do  # Skip the network and broadcast addresses
+    for i in $(seq 1 $((num_ips - 2))); do  
         local ip_num=$(($ip_int + i))
         local octet1=$(($ip_num >> 24 & 255))
         local octet2=$(($ip_num >> 16 & 255))
@@ -96,10 +94,10 @@ expand_cidr() {
     done
 }
 
-# Check if input is a CIDR and services can use it directly
+
 if echo "$INPUT" | grep -q "/"; then
     IS_CIDR=true
-    echo "CIDR detected, using appropriate services..."
+    # echo "CIDR detected, using appropriate services..."
     process_cidr $INPUT
     expand_cidr $INPUT
 else
