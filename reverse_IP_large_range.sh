603722e335b509f665035d4cd956cdb1930aea69
Network Sec <85315993+Network-Sec@users.noreply.github.com>
2025-01-17 18:51:52 +0100

    Update reverse_IP_large_range.sh

--- reverse_IP_large_range.sh
+++ reverse_IP_large_range.sh
@@ -9,5 +9,5 @@ then
         cat resolvers-extended.txt | cut -d " " -f 1 > public_dns_servers.txt
 fi
 
-# Edit the first number or the range start / end of each block
-parallel 'echo 123.{1}.{2}.{3}; dig -x $(echo 123.{1}.{2}.{3}) @{4}  +short | tee -a /IP_Ranges/residential_123.txt ' ::: $(seq 1 255) ::: $(seq 1 255) ::: $(seq 1 255) :::+ $(cat ./public_dns_servers.txt)
+# Edit the first number or the range start / end of each block - also adjust output folder / file
+parallel 'echo 123.{1}.{2}.{3}; dig -x $(echo 123.{1}.{2}.{3}) @{4}  +short | tee -a IP_Ranges/residential_123.txt ' ::: $(seq 1 255) ::: $(seq 1 255) ::: $(seq 1 255) :::+ $(cat ./public_dns_servers.txt)
